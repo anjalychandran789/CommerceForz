5c33f615563630be1484e0dbaac5d99a9c4a87ac
anjalychandran789 <48014476+anjalychandran789@users.noreply.github.com>
2022-05-24 12:01:36 +0530

    Add files via upload
    committ

---
+++ js/script.js
@@ -0,0 +1,189 @@
+/*alert("hello");
+
+// syntax of jquery ==  $(selector).action();
+// hide
+
+$('p').click(function(){
+    $('p').hide();
+});
+
+$('button').click(function(){
+    $('p').hide();
+});
+$('p').click(function(){
+    $('.text').hide();
+});
+
+Redy event
+-------------------
+$(document).ready(function(){
+    $('button').click(function(){
+        $('p').hide();
+    })
+})
+
+
+$(function(){
+    $('button').click(function(){
+        $('p').hide();
+    })
+})
+
+SELECTORS
+1)element selector
+2) class selector
+3) id selector
+$('button').click(function(){
+    $('#para').hide();
+});
+
+if you want to select all the element then use $('*') 
+if you want to select  1st para then $(para:first)
+hide(1000); used for set the time
+
+$(document).ready(function(){
+    $('button').click(function(){
+        //$('ul li:first').hide(1000);
+        //$('ul li:first-child').hide(1000);
+        //$('ul li:last').hide(1000);
+        $('ul li:last-child').hide(1000);
+    });
+});
+
+$(document).ready(function(){
+    $('button').click(function(){
+    $('[href]').hide(1000);
+    })
+
+})
+
+$(document).ready(function(){
+    $('button').click(function(){
+   console.log$('a[target="blank"]').hide(1000);
+    })
+
+})
+
+$(document).ready(function(){
+$('button').click(function(){
+    $('input').hide(1000);
+    })
+})
+
+$(document).ready(function(){
+$('button').click(function(){
+    $(':text').hide(1000);
+    })
+})
+$(document).ready(function(){
+$('button').click(function(){
+    consol.log($('h1').html("good evening"));
+    })
+})
+
+$(document).ready(function(){
+    $('button').click(function(){
+        console.log($(':text').val("good morning"));
+    })
+})
+
+$(document).ready(function(){
+    $('button').click(function(){
+        $('p').remove();
+    })
+})
+$(document).ready(function(){
+    $('button').click(function(){
+        $('p').empty();
+    })
+})
+
+$(document).ready(function(){
+    $('button').click(function(){
+        $('h1').addClass("heading");
+    })
+})
+
+$(document).ready(function(){
+    $('button').click(function(){
+        $('h1').addClass("htitle");
+    })
+})
+
+
+$(document).ready(function(){
+    $('button').click(function(){
+        $('h1').removeClass("htitle");
+    })
+})
+
+$(document).ready(function(){
+    $('button').click(function(){
+        console.log($('p').css('color'));
+    })
+})
+
+$(document).ready(function(){
+    $('button').click(function(){
+        console.log($('tr:odd').css('background-color','green'));
+    })
+})
+
+$(document).ready(function(){
+    $('button').mouseenter(function(){
+        console.log("mouse enter event");
+    })
+})
+
+$(document).ready(function(){
+    $('button').mouseleave(function(){
+        console.log("mouse enter event");
+    })
+})
+
+$(document).ready(function(){
+    $('button').mousedown(function(){
+        console.log("mouse enter event");
+    })
+})
+
+$(document).ready(function(){
+    $('button').mouseup(function(){
+        console.log("mouse enter event");
+    })
+})
+$(document).ready(function(){
+    $('button').hover(function(){
+        console.log("im enter");
+    },function(){
+        alert("im left");
+    })
+})
+$(document).ready(function(){
+$('input').focus(function(){
+        $(this).css("background-color","orang");
+})
+})
+
+$(document).ready(function(){
+$('input').focus(function(){
+        $(this).css("background-color","orang");
+        
+            $('input').blur(function(){
+                    $(this).css("background-color","orang");
+            })
+            })
+})
+
+
+
+$(document).ready(function(){
+$('input').focus(function(){
+        $(this).css("background-color","orang");
+        
+            $('input').blur(function(){
+                    $(this).css("background-color","orang");
+            })
+            })
+})
+*/
